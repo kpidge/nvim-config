df61c9477455cd81c99906c8d726594364e9e156
Kyle Pidgeon <92022089+kpidge@users.noreply.github.com>
2024-03-03 12:50:50 +0000

    Move plugin config to plugin/

--- init.lua
+++ init.lua
@@ -1,24 +1,5 @@
-require('remap') -- Needs to be first due to leader remap?
-require('plugins')
-require('lualine_config')
-require('nvimtree')
-require('treesitter')
-require('treesitter_ctx')
-require('telescope_config')
-require('lsp')
-require('null_ls')
-require('cmp_cfg')
-require('snippets')
-require('catppuccin_config')
-require('harpoon_config')
+require('remap')
 
-vim.cmd.colorscheme "catppuccin"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.cmd.filetype({ "plugin", "indent", "on" })
-
-function get_code_actions()
-	local range = {['start']={1,0}, ['end']={-1, -1}}
-	local diagnostics = vim.diagnostic.get()
-	return vim.lsp.buf.code_action({context = { diagnostics = diagnostics }, range = range })
-end
